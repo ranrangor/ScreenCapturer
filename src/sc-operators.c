79af89c7009e946f6dd71a69df7832341ab1c843
Ranrangor <ranrangor@gmail.com>
2014-05-15 17:13:51 +0800

    using image button..

--- src/sc-operators.c
+++ src/sc-operators.c
@@ -6,6 +6,8 @@
 #include"sc-shape.h"
 #include"sc-arrow.h"
 #include"sc-painter.h"
+#include"../icons/icons.h"
+#include"sc-utils.h"
 
 
 
@@ -76,19 +78,23 @@ void sc_canvas_register_operables(SCCanvas*canvas,GtkWidget*win)
 
     GtkWidget*operable_box=sc_canvas_get_operable_box(canvas);
 
-    GtkWidget*shape=gtk_button_new_with_label("shape");
+//    GtkWidget*shape=gtk_button_new_with_label("shape");
+    GtkWidget*shape=sc_image_button_new_by_size(icon_shape_circle,20);
     gtk_box_pack_end(GTK_BOX(operable_box),shape,FALSE,FALSE,0);
     g_signal_connect(G_OBJECT(shape),"clicked",G_CALLBACK(operator_shape_act),canvas);//&info);
 
-    GtkWidget*arrow=gtk_button_new_with_label("arrow");
+//    GtkWidget*arrow=gtk_button_new_with_label("arrow");
+    GtkWidget*arrow=sc_image_button_new_by_size(icon_arrow,20);
     gtk_box_pack_end(GTK_BOX(operable_box),arrow,FALSE,FALSE,0);
     g_signal_connect(G_OBJECT(arrow),"clicked",G_CALLBACK(operator_arrow_act),canvas);//&info);
 
-    GtkWidget*painter=gtk_button_new_with_label("painter");
+//    GtkWidget*painter=gtk_button_new_with_label("painter");
+    GtkWidget*painter=sc_image_button_new_by_size(icon_painter,20);
     gtk_box_pack_end(GTK_BOX(operable_box),painter,FALSE,FALSE,0);
     g_signal_connect(G_OBJECT(painter),"clicked",G_CALLBACK(operator_painter_act),canvas);//&info);
 
-    GtkWidget*text=gtk_button_new_with_label("text");
+//    GtkWidget*text=gtk_button_new_with_label("text");
+    GtkWidget*text=sc_image_button_new_by_size(icon_text,20);
     gtk_box_pack_end(GTK_BOX(operable_box),text,FALSE,FALSE,0);
     g_signal_connect(G_OBJECT(text),"clicked",G_CALLBACK(operator_text_act),canvas);//&info);
 
